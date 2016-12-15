96be83efafd236a37f95a67622093df9650f606d
Joseph Patrick LOVE <u1409433@uel.ac.uk>
2016-12-15 13:19:42 +0000

    Initial Project Upload

---
+++ Project/CW2/Assets/JL_Scripts/JL_Interactable.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.Networking;
+
+public class JL_Interactable : NetworkBehaviour
+{
+    public GameObject GO_Interactable;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    [Command]
+    public void CmdInteract(GameObject vPlayer)
+    {
+        JL_PCControl mSC_PC = vPlayer.GetComponent<JL_PCControl>();
+        if (gameObject.name == "Pickup")
+        {
+            gameObject.transform.SetParent(vPlayer.transform);
+            mSC_PC.SetChild(gameObject);
+        }
+        else if (gameObject.name == "Open Door")
+        {
+            gameObject.transform.DOMove(transform.position + new Vector3(0, 3, 0), 3);
+        }
+        /*else if (gameObject.name == "Locked Door")
+        {
+            if (mSC_PC.mBL_KeyHeld == true)
+            {
+                gameObject.transform.name = "Open Door";
+                mSC_PC.mBL_KeyHeld = false;
+                Debug.Log("Door Unlocked");
+            }
+        }*/
+        else if (gameObject.name == "Alarm Button")
+        {
+            foreach (GameObject Stormtrooper in GameObject.FindGameObjectsWithTag("StormTrooper"))
+            {
+                Stormtrooper.SendMessage("Alarm");
+            }
+        }
+        else if (gameObject.name == "Bridge Control")
+        {
+            Debug.Log("You Win");
+        }
+    }
+}
---
+++ Project/CW2/Assets/JL_Scripts/JL_NetworkManagerExtended.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class JL_NetworkManagerExtended : NetworkManager
+{
+    public static JL_NetworkManagerExtended Manager;
+
+    // Use this for initialization
+    void Start()
+    {
+        Manager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        switch (numPlayers)
+        {
+            case 0: playerPrefab = spawnPrefabs[0];
+                break;
+        }
+
+        base.OnServerAddPlayer(conn, playerControllerId);
+    }
+
+
+}
